c02563cfba05731b9b5b495a205fd2a757de2491
vagrant <vagrant@homestead>
2023-06-13 15:16:53 +0000

    optimized website

--- resources/views/easeweldo.blade.php
+++ resources/views/easeweldo.blade.php
@@ -4,7 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Easeweldo: Elevate Your Payroll Experience with Ease! - edited</title>
+        <title>Easeweldo: Elevate Your Payroll Experience with Ease!</title>
+
+        <!-- Meta Tags -->
+        <meta name="description" content="Easeweldo offers an exceptional payroll experience with ease and efficiency. Streamline your payroll process with our reliable solutions.">
+        <meta name="keywords" content="payroll, payroll software, payroll management, payroll system, payroll solutions">
+        <meta name="author" content="Easeweldo Team">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -15,6 +20,12 @@
         <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 
         <link rel="icon" href="/favicon.ico" type="image/x-icon">
+
+        <meta property="og:title" content="Easeweldo: Elevate Your Payroll Experience with Ease!">
+        <meta property="og:description" content="Easeweldo offers an exceptional payroll experience with ease and efficiency. Streamline your payroll process with our reliable solutions.">
+        <meta property="og:image" content="assets/images/home.png">
+        <meta property="og:url" content="https://easeweldo.tech">
+        <meta property="og:type" content="website">
     </head>
     <body class="antialiased">
         <style>
@@ -24,12 +35,15 @@
             .text-primary {
                 color: #5ba5f3;
             }
+            .bg-primary {
+                background: #5ba5f3
+            }
         </style>
         <main>
             <section class="hero bg-cover bg-center pt-16" style="background-image: url('assets/images/hero-2.png')">
                 <div class="container lg:mx-auto mx-1 flex items-center justify-between">
                     <div class="flex items-center">
-                        <img src="assets/images/logo.png" alt="Easeweldo Logo" class="h-12 w-auto"><h1 class="text-4xl lg:text-4xl font-bold ml-4">EASEweldo</h1>
+                        <img src="assets/images/logo.png" alt="Easeweldo Logo" class="h-12 w-auto"><h1 class="text-4xl lg:text-4xl font-bold ml-4">Easeweldo</h1>
                     </div>
                     <nav>
                         <ul class="flex space-x-4 hidden">
@@ -55,10 +69,10 @@
                 </div>
             </section>
             <section class="bg-white">
-                <div class="container mx-auto px-4 py-12">
+                <div class="container mx-auto px-4 py-16">
                     <div class="mb-12 mx-auto max-w-6xl">
                         <h2 class="text-4xl lg:text-6xl text-center font-bold mb-4 text-primary">What We Can Do For You</h2>
-                        <p class="text-lg lg:text-2xl text-center text-gray-700">With Easeweldo as your trusted payroll system, you can simplify your payroll processes, improve accuracy, ensure compliance, and gain valuable insights into your financials. Our aim is to empower your business by taking the burden of payroll management off your shoulders, allowing you to focus on what matters most – growing and nurturing your business.</p>
+                        <p class="text-lg lg:text-2xl text-center">With Easeweldo as your trusted payroll system, you can simplify your payroll processes, improve accuracy, ensure compliance, and gain valuable insights into your financials. Our aim is to empower your business by taking the burden of payroll management off your shoulders, allowing you to focus on what matters most – growing and nurturing your business.</p>
                     </div>
                     <div class="grid grid-cols-1 md:grid-cols-3 gap-8 pt-14">
                         <div class="bg-gray-200 rounded-lg p-8 flex flex-col items-center relative mt-0 lg:mt-2">
@@ -78,71 +92,64 @@
                         </div>
                     </div>
                 </div>
-                <div class="bg-blue-500 py-12">
+                <div class="bg-primary py-16">
                     <div class="container mx-auto px-4">
                         <div class="flex flex-wrap justify-between">
                             <div class="w-full md:w-2/5 pb-4">
-                                <h2 class="text-4xl lg:text-6xl font-bold mb-6 lg:hidden block text-white">Get to Know Us Better</h2>
+                                <h2 class="text-4xl lg:text-6xl font-bold mb-8 lg:hidden block text-white">Get to Know Us Better</h2>
                                 <div class="image-container">
                                 <img src="assets/images/overview.png" alt="Image description" class="w-full">
                                 </div>
                             </div>
                             <div class="w-full md:w-3/5">
-                                <div class="text-white pl-0 md:pl-12">
-                                    <h2 class="text-4xl lg:text-6xl font-bold mb-6 hidden lg:block">Get to Know Us Better</h2>
-                                    <hr class="mb-6 border-white-300 border-t-4 w-20 hidden lg:block">
-                                    <p class="mb-6 text-lg text-lg lg:text-2xl text-justify">At Easeweldo, we understand the challenges and complexities of managing payroll for your business. We are more than just a payroll system; we are your trusted partner in simplifying the payroll process and ensuring the smooth operation of your company.</p>
-                                    <p class="mb-6 text-lg text-lg lg:text-2xl text-justify">We are dedicated to providing you with a seamless payroll management experience, saving you valuable time and effort. With Easeweldo, you can rely on our state-of-the-art technology to automate calculations, tax deductions, and benefit contributions, eliminating the need for manual tasks and reducing the risk of errors. We are committed to staying up-to-date with the latest labor laws and tax regulations, ensuring your payroll remains compliant.</p>
-                                    <p class="mb-6 text-lg text-lg lg:text-2xl text-justify">Our goal is to empower you to focus on what matters most – growing your business and nurturing your employees, while we take care of your payroll needs. Trust Easeweldo to simplify your payroll management to help your business thrive.</p>
+                                <div class="text-white pl-0 md:pl-12 pt-10">
+                                    <h2 class="text-4xl lg:text-6xl font-bold mb-8 hidden lg:block">Get to Know Us Better</h2>
+                                    <hr class="mb-8 border-white-300 border-t-4 w-20 hidden lg:block">
+                                    <p class="mb-8 text-lg text-lg lg:text-2xl text-justify">At Easeweldo, we understand the challenges and complexities of managing payroll for your business. We are more than just a payroll system; we are your trusted partner in simplifying the payroll process and ensuring the smooth operation of your company.</p>
+                                    <p class="mb-8 text-lg text-lg lg:text-2xl text-justify">We are dedicated to providing you with a seamless payroll management experience, saving you valuable time and effort. With Easeweldo, you can rely on our state-of-the-art technology to automate calculations, tax deductions, and benefit contributions, eliminating the need for manual tasks and reducing the risk of errors. We are committed to staying up-to-date with the latest labor laws and tax regulations, ensuring your payroll remains compliant.</p>
+                                    <p class="mb-8 text-lg text-lg lg:text-2xl text-justify">Our goal is to empower you to focus on what matters most – growing your business and nurturing your employees, while we take care of your payroll needs. Trust Easeweldo to simplify your payroll management to help your business thrive.</p>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="container mx-auto py-12">
-                    <h2 class="text-4xl lg:text-6xl text-center font-bold mb-4 text-primary">Why Us?</h2>
+                <div class="container mx-auto py-16">
+                    <h2 class="text-4xl lg:text-6xl text-center font-bold mb-14 text-primary">Why Us?</h2>
                     <div class="flex flex-wrap -mx-4">
-                        <div class="w-full sm:w-1/2 md:w-1/3 px-4 mb-8">
-                            <div class="flex items-center mb-4">
-                                <img src="assets/images/reliability.png" alt="Reliability" class="w-8 h-8 mr-4">
-                                <h3 class="text-lg font-bold">Reliability</h3>
+                        <div class="w-full sm:w-1/2 md:w-1/3 px-4 mb-8 my-10">
+                            <div class="rounded-lg flex flex-col items-center relative mt-0 lg:mt-2">
+                                <img src="assets/images/reliability.png" alt="Reliability" class="w-56 h-56">
+                                <h3 class="text-3xl font-bold my-7">Reliability</h3>
+                                <p class="text-xl font-semibold text-center w-64">Count on us to deliver consistent and accurate results.</p>
                             </div>
-                            <p>Count on us to deliver consistent and accurate results.</p>
+                        </div>
+                        <div class="w-full sm:w-1/2 md:w-1/3 px-4 mb-8 my-10">
+                            <div class="rounded-lg flex flex-col items-center relative mt-0 lg:mt-2">
+                                <img src="assets/images/simplicity.png" alt="Simplicity" class="w-56 h-56">
+                                <h3 class="text-3xl font-bold my-7">Simplicity</h3>
+                                <p class="text-xl font-semibold text-center w-64">Driven to create an intuitive and user-friendly payroll system.</p>
                             </div>
-                            <div class="w-full sm:w-1/2 md:w-1/3 px-4 mb-8">
-                            <div class="flex items-center mb-4">
-                                <img src="assets/images/simplicity.png" alt="Simplicity" class="w-8 h-8 mr-4">
-                                <h3 class="text-lg font-bold">Simplicity</h3>
+                        </div>
+                        <div class="w-full sm:w-1/2 md:w-1/3 px-4 mb-8 my-10">
+                            <div class="rounded-lg flex flex-col items-center relative mt-0 lg:mt-2">
+                                <img src="assets/images/partnership.png" alt="Partnership" class="w-56 h-56">
+                                <h3 class="text-3xl font-bold my-7">Partnership</h3>
+                                <p class="text-xl font-semibold text-center w-64">We see ourselves as more than just a service provider; we are your trusted partner.</p>
                             </div>
-                            <p>Driven to create an intuitive and user-friendly payroll system.</p>
-                            </div>
-                            <div class="w-full sm:w-1/2 md:w-1/3 px-4 mb-8">
-                            <div class="flex items-center mb-4">
-                                <img src="assets/images/partnership.png" alt="Partnership" class="w-8 h-8 mr-4">
-                                <h3 class="text-lg font-bold">Partnership</h3>
-                            </div>
-                            <p>We see ourselves as more than just a service provider; we are your trusted partner.</p>
                         </div>
                     </div>
                 </div>
-                <div class="bg-blue-500 py-12">
-                    <div class="container mx-auto">
-                        <h2 class="text-3xl font-bold text-white mb-8">Let us help you</h2>
-                        <p class="text-white mb-8">Reach out for an exploratory conversation.</p>
-                        <div class="flex flex-wrap items-center justify-center">
-                        <div class="flex items-center mr-8 mb-4">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-white mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 15c0-1.657 1.79-4 4-4h8c2.21 0 4 2.343 4 4v4c0 1.657-1.79 4-4 4H8c-2.21 0-4-2.343-4-4v-4z" />
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 10V3m0 0L9 6m3-3l3 3M12 21v-2" />
-                            </svg>
-                            <span class="text-white">123-456-7890</span>
+                <div class="bg-primary py-16">
+                    <div class="container mx-auto text-white px-4">
+                        <h2 class="text-4xl lg:text-6xl font-bold mb-3">Let Us Help You</h2>
+                        <p class="text-lg lg:text-2xl mb-14">Reach out for an exploratory conversation.</p>
+                        <div class="flex items-center mb-4">
+                            <img src="assets/images/phone-icon.png" alt="Phone" class="w-10 h-10 mr-4">
+                            <p class="text-2xl font-semibold"><a href="tel:+123456789">123-456-789</a></p>
                         </div>
                         <div class="flex items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-white mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 22C6.486 22 2 17.514 2 12c0-5.514 4.486-10 10-10s10 4.486 10 10c0 5.514-4.486 10-10 10zm0 0l-4-4m4 4l4-4m-4 4V4" />
-                            </svg>
-                            <span class="text-white">dummyemail@example.com</span>
-                        </div>
+                            <img src="assets/images/email-icon.png" alt="Email" class="w-10 h-10 mr-4">
+                            <p class="text-2xl font-semibold"><a href="mailto:info@example.com">info@example.com</a></p>
                         </div>
                     </div>
                 </div>
