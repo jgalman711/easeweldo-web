2b5bc59e15b35a7c096a65a5d5e5a0b802815a6e
vagrant <vagrant@homestead>
2023-08-22 16:41:56 +0000

    fixed api endpoint

--- app/Services/HttpService.php
+++ app/Services/HttpService.php
@@ -6,15 +6,19 @@ use Illuminate\Support\Facades\Http;
 
 class HttpService
 {
-    public $apiEndpoint = "http://api-sahodna.test/api/";
+    protected $endpoint;
+
+    public function __construct()
+    {
+        $this->endpoint = env('EASEWELDO_API_DOMAIN');
+    }
 
     public function get(string $uri)
     {
-        $uri = $this->apiEndpoint . $uri;
+        $uri = $this->endpoint . $uri;
         $response = Http::get($uri)->json();
         if (isset($response['success']) && $response['success']) {
             return $response['data'];
         }
     }
-    
 }
