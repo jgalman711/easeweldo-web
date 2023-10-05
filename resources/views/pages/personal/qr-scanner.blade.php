27b94dcaa8a95d133fb9ffb8ec58453d632e6aab
vagrant <vagrant@homestead>
2023-10-05 18:36:36 +0000

    qr scanner

--- resources/views/pages/personal/qr-scanner.blade.php
+++ resources/views/pages/personal/qr-scanner.blade.php
@@ -21,8 +21,7 @@
 <script>
 function onScanSuccess(decodedText, decodedResult) {
     // Handle on success condition with the decoded text or result.
-    alert(decodedText);
-    alert(decodedResult);
+    html5QrcodeScanner.clear();
 }
 
 var html5QrcodeScanner = new Html5QrcodeScanner(
